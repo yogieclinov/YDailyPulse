30a5b1efcd219170ff2c5e3c38a43eadf86ad413
Yogie Clinov <yogie.clinov01@gmail.com>
2025-04-20 21:20:04 +0700

    add 60 minute expired cache time

--- shared/src/commonMain/kotlin/com/clinov/ydailypulse/data/datasource/ArticlesLocalDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/com/clinov/ydailypulse/data/datasource/ArticlesLocalDataSourceImpl.kt
@@ -2,17 +2,39 @@ package com.clinov.ydailypulse.data.datasource
 
 import com.clinov.ydailypulse.data.model.ArticleDbEntity
 import com.clinov.ydailypulse.db.YDailyPulseDatabase
+import kotlinx.datetime.Clock
 
 class ArticlesLocalDataSourceImpl(
     private val database: YDailyPulseDatabase
 ) : ArticlesLocalDataSource {
 
-    override fun getHeadlineArticlesDb(): List<ArticleDbEntity> =
-        database.ydailypulsedbQueries.selectAllArticles(::mapToArticleDbEntity).executeAsList()
+    private val cacheThreshold = 1000 * 60 * 60 // 60 minutes cache expired
+    private val headlineArticlesTableName = "HeadlineArticle"
+
+    private fun getCurrentTimeInMillis() = Clock.System.now().toEpochMilliseconds()
+
+    override fun getHeadlineArticlesDb(): List<ArticleDbEntity> {
+        val tableAccess = database.ydailypulsetablemanagerdbQueries.getTable(headlineArticlesTableName).executeAsOneOrNull()
+        return if (isExpired(tableAccess?.lastUpdated)) {
+            clearHeadlineArticlesDb()
+            emptyList()
+        } else {
+            database.ydailypulsedbQueries.selectAllArticles(::mapToArticleDbEntity).executeAsList()
+        }
+    }
+
+    private fun isExpired(lastUpdate: Long?): Boolean {
+        if (lastUpdate == null) return true
+        return getCurrentTimeInMillis() - lastUpdate > cacheThreshold
+    }
+
 
     override fun insertHeadlineArticlesDb(articles: List<ArticleDbEntity>) {
         database.ydailypulsedbQueries.transaction {
             articles.forEach { insertArticle(it) }
+            database.ydailypulsetablemanagerdbQueries.insertTableAccess(
+                headlineArticlesTableName, getCurrentTimeInMillis()
+            )
         }
     }
 
